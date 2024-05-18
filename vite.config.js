7db30dc76a2b8261a29962b88f2214f87ea2fc14
adbhut96 <100306370+adbhut96@users.noreply.github.com>
2024-05-18 17:05:07 +0530

    fixed: removed port number from the api url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // eslint-disable-next-line no-unused-vars
 export default defineConfig(({ mode}) => {
   // eslint-disable-next-line no-undef
-  const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.meetello.live:8000' : 'http://localhost:8000';
+  const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.meetello.live' : 'http://localhost:8000';
   return {
     plugins: [react()],
     server: {
